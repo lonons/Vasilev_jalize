2df8f4fc4ff32b43fb21eacd342d58fec1ca87a5
lonons <vkolay01@mail.ru>
2022-04-11 22:35:40 +0700

    task5

--- Assets/MyProject/Scripts/Enemy/Enemy_controller.cs
+++ Assets/MyProject/Scripts/Enemy/Enemy_controller.cs
@@ -10,16 +10,32 @@ namespace jalize
 
         private NavMeshAgent agent;
         private Transform player;
+        private LayerMask _mask ;
+
 
         private void Awake()
         {
+            _mask = LayerMask.NameToLayer("Player");
             agent = GetComponent<NavMeshAgent>();
             player = GameObject.FindGameObjectWithTag("Player").transform;
         }
-
-        private void Update()
+        private void OnTriggerStay(Collider other)
         {
-            agent.SetDestination(player.transform.position);
+            if (other.gameObject.CompareTag("Player"))
+            {
+                RaycastHit hit;
+                var startPos = transform.position;
+                var dir = player.position - startPos;
+                var rayCast = Physics.Raycast(startPos, dir, out hit, Mathf.Infinity, _mask);
+                if (rayCast)
+                {
+                    if (hit.collider.gameObject.CompareTag("Player"))
+                    {
+                        agent.SetDestination(player.transform.position);
+                    }
+                }
+                Debug.DrawRay(startPos, dir);
+            }
         }
 
     }
